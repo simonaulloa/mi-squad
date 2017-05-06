8a1243badf683488d81ce5417e2d5e879eae9714
simona <simonaulloa@gmail.com>
2017-05-05 21:26:00 -0300

    arreglo2

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,26 +18,11 @@ var arr = [];
 	var maibeet = new MiembroSquad("Maibeet Torres", 15, ["Jugar juegos de RPG", "Leer", "dormir"]);
 		arr.push(maibeet);
 
-	arr.forEach(function(ele){
-		innerHTML("nombre: " + ele.nombre + "edad: " + ele.edad);
-		
-		ele.hobbies.forEach(function(e){
-			innerHTML("hobbies: " + e);
-		})
-		
-	})
+var imprimir = document.getElementById("verGrupo");
 
+arr.forEach(function(elementos){
 
-
-/*var Carol = document.getElementById("caro");
-caro.innerHTML = "<strong>Nombre:</strong> " + carolina.nombre + "<strong><br>Edad:</strong> " + carolina.edad + "<strong><br>Hobbies:</strong> " + carolina.hobbies;
-var Murie = document.getElementById("muri");
-muri.innerHTML = "<strong>Nombre:</strong> " + muriel.nombre + "<strong><br>Edad:</strong> " + muriel.edad + "<strong><br>Hobbies:</strong> " + muriel.hobbies;
-var An = document.getElementById("nanyn");
-nanyn.innerHTML = "<strong>Nombre:</strong> " + anayn.nombre + "<strong><br>Edad:</strong> " + anayn.edad + "<strong><br>Hobbies:</strong> " + anayn.hobbies;
-var Josef = document.getElementById("jose");
-jose.innerHTML = "<strong>Nombre:</strong> " + josefa.nombre + "<strong><br>Edad:</strong> " + josefa.edad + "<strong><br>Hobbies:</strong> " + josefa.hobbies;
-var A = document.getElementById("ana");
-ana.innerHTML = "<strong>Nombre:</strong> " + anais.nombre + "<strong><br>Edad:</strong> " + anais.edad + "<strong><br>Hobbies:</strong> " + anais.hobbies;
-var Maib = document.getElementById("mai");
-mai.innerHTML = "<strong>Nombre:</strong> " + maibeet.nombre + "<strong><br>Edad:</strong> " + maibeet.edad + "<strong><br>Hobbies:</strong> " + maibeet.hobbies;
+    imprimir.innerHTML +=  '<b>Nombre:</b> ' + elementos.nombre + '<br><b>Edad:</b> ' + elementos.edad + '<br><b>Hobbies:</b><br>';
+    var lista = '<ul> ' + elementos.hobbies.forEach(function(h){return (imprimir.innerHTML += "<li>" + h + "</li>")}) + '</ul>';
+    imprimir.innerHTML += '<br>' ;
+})
